adad440909c5dd8bd572285315c233ea2ca6fdde
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-07-29 07:50:31 -0400

    Inital commit getting the stage set

---
+++ readme.md
@@ -0,0 +1,3 @@
+# SPINEN's base Laravel install
+
+This is a Laravel 5 middleware to filter routes based on browser types.
---
+++ tests/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Spinen\BrowserFilter;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class TestCase
+ *
+ * @package Tests\Spinen\BrowserFilter
+ * @group base
+ */
+class TestCase extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        if (class_exists('Mockery')) {
+            Mockery::close();
+        }
+
+        parent::tearDown();
+    }
+}

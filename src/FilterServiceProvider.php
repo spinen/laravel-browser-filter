fa6b479062de7384e2b74ed0683e58728f7b2ab9
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-08-01 17:46:11 -0400

    Triggering HiSoOrange package

--- src/FilterServiceProvider.php
+++ src/FilterServiceProvider.php
@@ -3,9 +3,27 @@
 namespace Spinen\BrowserFilter;
 
 use Illuminate\Support\ServiceProvider;
+use hisorange\BrowserDetect\Provider\BrowserDetectService;
 
+/**
+ * Class FilterServiceProvider
+ *
+ * @package Spinen\BrowserFilter
+ */
 class FilterServiceProvider extends ServiceProvider
 {
+    /**
+     * @var BrowserDetectService
+     */
+    protected $browser_detect;
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
     /**
      * Perform post-registration booting of services.
      *
@@ -13,6 +31,8 @@ class FilterServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->bootBrowserDetect();
+
         $config_file = realpath(__DIR__ . '/config/browserfilter.php');
 
         $this->publishes([
@@ -23,12 +43,30 @@ class FilterServiceProvider extends ServiceProvider
     }
 
     /**
+     *  Preform post-registration booting of the Browser Detect service.
+     */
+    private function bootBrowserDetect()
+    {
+        $this->browser_detect->boot();
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
      */
     public function register()
     {
-        //
+        $this->registerBrowserDetect();
+    }
+
+    /**
+     * Register the Browser Detect service.
+     */
+    private function registerBrowserDetect()
+    {
+        $this->browser_detect = $this->app->make(BrowserDetectService::class);
+
+        $this->browser_detect->register();
     }
 }

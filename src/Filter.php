202b68053dc307d85a01b6aa5dd5e17dd7ffa267
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-08-09 10:20:18 -0400

    Filtering with UAParser

--- src/Filter.php
+++ src/Filter.php
@@ -5,6 +5,8 @@ namespace Spinen\BrowserFilter;
 use Closure;
 use Illuminate\Contracts\Config\Repository as Config;
 use Illuminate\Http\Request;
+use Mobile_Detect;
+use UAParser\Parser;
 
 /**
  * Class Filter
@@ -14,6 +16,11 @@ use Illuminate\Http\Request;
 class Filter
 {
     /**
+     * @var \UAParser\Result\Client
+     */
+    private $client;
+
+    /**
      * The config repository instance.
      *
      * @var Configs
@@ -21,13 +28,50 @@ class Filter
     protected $config;
 
     /**
+     * @var string
+     */
+    protected $config_path = 'browserfilter.blocked.';
+
+    /**
+     * The mobile detector instance.
+     *
+     * @var Mobile_Detect
+     */
+    private $detector;
+
+    /**
      * Create a new browser filter middleware instance.
      *
-     * @param Config $config Config
+     * @param Config        $config   Config
+     * @param Mobile_Detect $detector Mobile_Detect
      */
-    public function __construct(Config $config)
+    public function __construct(Config $config, Mobile_Detect $detector)
     {
         $this->config = $config;
+        $this->detector = $detector;
+        // TODO: Look into way to not call statically & inject it
+        $this->client = Parser::create()
+                              ->parse($this->detector->getUserAgent());
+    }
+
+    /**
+     * Get the browsers being filtered.
+     *
+     * @return string|array
+     */
+    private function getBlockedBrowsers()
+    {
+        return $this->config->get($this->config_path . $this->client->device->family);
+    }
+
+    /**
+     * Get the versions of the browsers being filtered.
+     *
+     * @return string|array
+     */
+    private function getBlockedBrowserVersions()
+    {
+        return $this->config->get($this->config_path . $this->client->device->family . '.' . $this->client->ua->family);
     }
 
     /**
@@ -40,8 +84,65 @@ class Filter
      */
     public function handle(Request $request, Closure $next)
     {
-        // Do whatever work needed
+        // TODO: Wrap this with the cache repository
+        if ($this->isBlocked()) {
+            dd("Denied");
+            // TODO: Return proper view here that is configurable
+        }
 
         return $next($request);
+    }
+
+    /**
+     * Checks to see if the browser/client is blocked.
+     *
+     * @return bool
+     */
+    private function isBlocked()
+    {
+        return ($this->isBlockedDevice() || $this->isBlockedBrowser() || $this->isBlockedBrowserVersion());
+    }
+
+    /**
+     * Checks to see if all versions of the browser is blocked.
+     *
+     * @return bool
+     */
+    private function isBlockedBrowser()
+    {
+        return $this->getBlockedBrowserVersions() === '*';
+    }
+
+    /**
+     * Checks to see if the version of the browser is blocked.
+     *
+     * Uses the php version_compare function to decide if there is a match.
+     *
+     * @link http://php.net/manual/en/function.version-compare.php
+     *
+     * @return bool
+     */
+    private function isBlockedBrowserVersion()
+    {
+        $denied = false;
+
+        // cache it, so that we don't have to keep asking for it
+        $client_version = $this->client->ua->toVersion();
+
+        foreach ((array)$this->getBlockedBrowserVersions() as $operator => $version) {
+            $denied |= (bool)version_compare($client_version, $version, $operator);
+        }
+
+        return $denied;
+    }
+
+    /**
+     * Checks to see if all browsers of the device family is blocked.
+     *
+     * @return bool
+     */
+    private function isBlockedDevice()
+    {
+        return $this->getBlockedBrowsers() === '*';
     }
 }

95e6ced3fedd9ca7a5f9918fcad104461c0ab335
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-08-15 21:51:17 -0400

    WIP: Starting the allow/block middlewares

---
+++ src/RouteBlockFilter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Spinen\BrowserFilter;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RouteBlockFilter extends RouteFilter
+{
+    /**
+     * @inheritDoc
+     */
+    function process(Request $request, Closure $next, array $filter)
+    {
+        dd($filter);
+        //TODO: Write the logic here
+    }
+}

3daac7ed8293af19beb4db8a05af36d90945823f
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-08-23 17:44:28 -0400

    Improving test

--- tests/Route/FilterTest.php
+++ tests/Route/FilterTest.php
@@ -126,8 +126,149 @@ class FilterTest extends TestCase
     /**
      * @test
      */
-    public function it_can_do_other_stuff()
+    public function it_parses_a_filter_string_where_it_understands_to_default_remaining_parameters()
     {
-        $this->markTestIncomplete("Write these tests");
+        $filter_string = 'First;Second/Third;Fourth/Fifth/6';
+        $expected = [
+            'First'  => '*',
+            'Second' => [
+                'Third' => '*',
+            ],
+            'Fourth' => [
+                'Fifth' => [
+                    '=' => '6',
+                ],
+            ],
+        ];
+
+        $this->filter->parseFilterString($filter_string);
+
+        $this->assertEquals($expected, $this->filter->getRules());
+    }
+
+    /**
+     * @test
+     */
+    public function it_parses_a_filter_string_and_ignores_null_values()
+    {
+        $filter_string = 'First/;Second/Third/;Fourth/Fifth/6|;';
+        $expected = [
+            'First'  => '*',
+            'Second' => [
+                'Third' => '*',
+            ],
+            'Fourth' => [
+                'Fifth' => [
+                    '=' => '6',
+                ],
+            ],
+        ];
+
+        $this->filter->parseFilterString($filter_string);
+
+        $this->assertEquals($expected, $this->filter->getRules());
+    }
+
+    /**
+     * @test
+     */
+    public function it_merges_values_when_parsing_a_filter_string()
+    {
+        $filter_string = 'First/Second/<=3;First/Second/>4;First/Fifth';
+        $expected = [
+            'First' => [
+                'Second' => [
+                    '<=' => '3',
+                    '>'  => '4',
+                ],
+                'Fifth'  => '*',
+            ],
+        ];
+
+        $this->filter->parseFilterString($filter_string);
+
+        $this->assertEquals($expected, $this->filter->getRules());
+    }
+
+    /**
+     * @test
+     */
+    public function it_allows_a_star_or_default_to_override_existing()
+    {
+        $filter_string = 'First/Second/<=2;First/Second;Third/Forth/=3;Third';
+        $expected = [
+            'First' => [
+                'Second' => '*',
+            ],
+            'Third' => '*',
+        ];
+
+        $this->filter->parseFilterString($filter_string);
+
+        $this->assertEquals($expected, $this->filter->getRules());
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_the_correct_rules_for_the_device()
+    {
+        $filter_string = 'First/Second/<=3;First/Second/>4;First/Fifth';
+        $rules = [
+            'First' => [
+                'Second' => [
+                    '<=' => '3',
+                    '>'  => '4',
+                ],
+                'Fifth'  => '*',
+            ],
+        ];
+
+        $this->filter->parseFilterString($filter_string);
+
+        $device = 'First';
+        $this->client_device_mock->family = $device;
+
+        $this->assertEquals($rules[$device], $this->filter->getBlockedBrowsers());
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_the_correct_rules_for_the_browser()
+    {
+        $filter_string = 'First/Second/<=3;First/Second/>4;First/Fifth';
+        $rules = [
+            'First' => [
+                'Second' => [
+                    '<=' => '3',
+                    '>'  => '4',
+                ],
+                'Fifth'  => '*',
+            ],
+        ];
+
+        $this->filter->parseFilterString($filter_string);
+
+        $device = 'First';
+        $ua = 'Second';
+
+        $this->client_device_mock->family = $device;
+        $this->client_ua_mock->family = $ua;
+
+        $this->assertEquals($rules[$device][$ua], $this->filter->getBlockedBrowserVersions());
+    }
+
+    /**
+     * @test
+     */
+    public function it_uses_the_configuration_redirect_route_as_the_default()
+    {
+        $this->config_mock->shouldReceive('get')
+                          ->once()
+                          ->with('browserfilter.route')
+                          ->andReturn('route');
+
+        $this->assertEquals('route', $this->filter->getRedirectRoute());
     }
 }

838ccf644214faa33dda4aa343b2a1b0a97cdc65
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-08-27 08:20:16 -0400

    Making the filter stub have some setters to allow the testing

--- tests/Stubs/FilterStub.php
+++ tests/Stubs/FilterStub.php
@@ -18,14 +18,50 @@ class FilterStub extends Filter
      */
     public function parseFilterString($filter_string)
     {
-        $this->rules = $filter_string;
+        return;
     }
 
     /**
      * @inheritDoc
      */
-    function process(Request $request, Closure $next)
+    public function process(Request $request, Closure $next)
     {
         return "Stub";
+    }
+
+    /**
+     * Method in the stub to allow you to set the block_filter for testing.
+     *
+     * @param bool $block_filter Set type
+     *
+     * @return void
+     */
+    public function setBlockFilter($block_filter)
+    {
+        $this->block_filter = $block_filter;
+    }
+
+    /**
+     * Method in the stub to allow you to set the redirect_route for testing.
+     *
+     * @param string $redirect_route Set the name of the route to redirect
+     *
+     * @return void
+     */
+    public function setRedirectRoute($redirect_route)
+    {
+        $this->redirect_route = $redirect_route;
+    }
+
+    /**
+     * Method in the stub to allow you to set the rules for testing.
+     *
+     * @param array $rules Array of rules
+     *
+     * @return void
+     */
+    public function setRules(array $rules)
+    {
+        $this->rules = $rules;
     }
 }

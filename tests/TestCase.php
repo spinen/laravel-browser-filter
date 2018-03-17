8b583ae50dbac3958fb7931a91a0a3b1553dd58e
Jimmy Puckett <jimmy.puckett@spinen.com>
2018-03-16 20:02:06 -0400

    Reconfigure to use latest phpunit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,14 +3,14 @@
 namespace Spinen\BrowserFilter;
 
 use Mockery;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 
 /**
  * Class TestCase
  *
  * @package Spinen\BrowserFilter
  */
-abstract class TestCase extends PHPUnit_Framework_TestCase
+abstract class TestCase extends PHPUnitTestCase
 {
     public function tearDown()
     {
